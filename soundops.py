f6894143dbb7937554baf0fd970daabf90d3d557
David SC <d.santa.2021@alumnos.urjc.es>
2023-10-07 21:17:34 +0200

    Ejercicio7

--- soundops.py
+++ soundops.py
@@ -9,6 +9,7 @@ def soundadd(s1: Sound, s2: Sound) -> Sound:
     if len(s1.buffer) == len(s2.buffer):
         s3.buffer = list(map(add, s1.buffer, s2.buffer))
     else:
-        s3.buffer = list(sum(x) for x in zip_longest(s1.buffer, s2.buffer, fillvalue=0))
+        s3.buffer = list(sum(x) for x in zip_longest(s1.buffer,
+                                                     s2.buffer, fillvalue=0))
 
     return s3
